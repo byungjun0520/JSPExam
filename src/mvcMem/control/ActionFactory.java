37aea52a86ffb8d2e0b61eacc8f498022426424b
byungjun0520 <byungjun0520@gmail.com>
2020-08-11 10:23:44 +0900

    mvcMem

---
+++ src/mvcMem/control/ActionFactory.java
@@ -0,0 +1,39 @@
+package mvcMem.control;
+
+import mvcMem.action.Action;
+import mvcMem.action.IndexAction;
+
+
+public class ActionFactory {
+
+	
+private static ActionFactory factory;
+	
+	private ActionFactory() {	}
+	
+	
+	public static synchronized ActionFactory getInstance() {
+		if( factory == null) {
+			factory = new ActionFactory();
+		}
+	return factory;	
+	}
+	
+	public Action getAction(String cmd) {
+		Action action = null;
+		
+		switch(cmd) {
+		
+		case "index" :
+			action = new IndexAction();
+		break;
+		
+		
+		
+		
+		}
+	
+		return action;
+	}
+	
+}
